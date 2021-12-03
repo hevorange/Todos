805dcf7129b3172a907371c22396f3cec3b47aa2
H3VO <hevonox@gmail.com>
2021-12-02 20:18:31 -0600

    insertamos los todos en la caja del HTML

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 import './styles.css';
 import {Todo, TodoList} from './classes';
+import { crearTodoHtml } from './js/componentes';
 
 
 
-const tarea = new Todo('aprender JavaScript');
-const todoList = new TodoList();
+const tarea = new Todo('aprender PLatzi');
+export const todoList = new TodoList();
 
 todoList.nuevoTodo(tarea);
 
 console.log(todoList);
+
+crearTodoHtml(tarea);
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,14 +1,40 @@
-import '../css/componentes.css';
+import {Todo} from '../classes';
+import {todoList} from '../index';
+//REFERENCIAS EN HTML
+
+const divTodoList = document.querySelector('.todo-list');
+const txtInput = document.querySelector('.new-todo');
 
 
+export const crearTodoHtml =(todo)=>{
 
-export const saludar = ( nombre ) => {
+    const htmlTodo = `
+    <li class="${(todo.completado)? 'completed' :''}" data-id="${todo.id}">
+        <div class="view">
+            <input class="toggle" type="checkbox" ${(todo.completado)? 'checked': ''}>
+            <label>${todo.tarea}</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
+    </li>`;
 
-    console.log('Creando etiqueta h1, en el HTML!');
+    const div = document.createElement('div');
+    div.innerHTML=htmlTodo;
 
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${ nombre }`;
+    divTodoList.append(div.firstElementChild);
 
-    document.body.append( h1 );
+    return div.firstElementChild;
 
 }
+
+txtInput.addEventListener('keyup',(event)=>{
+
+    if (event.keyCode===13 && txtInput.value.length>0) {
+
+        const nuevoTodo  = new Todo(txtInput.value);
+        todoList.nuevoTodo(nuevoTodo);
+        crearTodoHtml(nuevoTodo);
+        txtInput.value='';
+        
+    }
+})
